2a66dcb1a6c32de36d4fdbb73da857db5e382615
Brett <brett_matsumoto@yahoo.com>
2019-03-09 15:16:37 -1000

    completed scope

--- scope.js
+++ scope.js
@@ -3,10 +3,12 @@ var isCookieJarOpen = null;
 
 function closeLid() {
   /* answer here */
+  isCookieJarOpen = false;
 }
 
 function openLid() {
   /* answer here */
+  isCookieJarOpen = true;
 }
 
 
@@ -17,7 +19,7 @@ function outerFunction() {
 
   function innerFunction() {
     var world = 'World';
-    return /* answer here */;
+    return hello + ' ' + world;
   }
   return innerFunction();
 }
@@ -31,25 +33,26 @@ function addMatrixElements(matrix) {
 
   for(var i = 0; i < matrix.length; i++) {
     /* fix counter variables in the second loop */
-    for(var i = 0; i < matrix[i].length; i++) {
-      result = result + matrix[i][i];
+    for(var j = 0; j < matrix[i].length; j++) {
+      result = result + matrix[i][j];
     }
   }
   return result;
 }
 
 
+
 // This function is returning the wrong userObject data. It should be returning
 // Neo's information and not Morpheus'.  Fix it!
 function sendDataToClient() {
 
-  var userObject = {
+  let userObject = {
     handle: 'neo',
     authenticated: false
   }
 
   function authenticateUser(obj, username) {
-    var userObject = {
+    var obj = {
       handle: 'morpheus',
       authenticated: false
     };
@@ -58,7 +61,7 @@ function sendDataToClient() {
       userObject.authenticated = true;
       return userObject
     } else {
-      return userObject
+      return obj
     }
   }
   authenticateUser(userObject, 'neo')
